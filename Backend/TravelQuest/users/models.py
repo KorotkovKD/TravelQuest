df43a08ae7eda5a34b970428954f6608dc10efa4
Константин Коротков <KorotkovKD@yandex.ru>
2024-03-10 15:47:38 +0300

    Custom user

--- Backend/TravelQuest/users/models.py
+++ Backend/TravelQuest/users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    email = models.EmailField(
+        verbose_name="Почта  пользователя", unique=True, max_length=254
+    )
+
+    class Meta:
+        db_table = "auth_user"
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        indexes = (models.Index(fields=("email",), name="email_idx"),)
+        ordering = ("-id",)
+
+    @property
+    def is_authenticated(self):
+        return True
